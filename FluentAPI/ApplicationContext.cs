a2a90282025398c865b1efa5b372dffe47f7cabf
EightLegs <esmitin17@mail.ru>
2022-03-28 18:06:49 +0300

    Fluent Api

---
+++ FluentAPI/ApplicationContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FluentAPI
+{
+    public class ApplicationContext : DbContext
+    {
+        //public DbSet<User> Users { get; set; } = null!;
+        public DbSet<User> Users => Set<User>();
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // использование Fluent API
+            //Не создаем столбец
+            modelBuilder.Entity<User>().Ignore(u => u.Address);
+            //Создание сущности Country
+            //modelBuilder.Entity<Country>();
+            //Исключение сущности Company
+            //modelBuilder.Ignore<Company>();
+            // использование Fluent API
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FluentAPI/Tables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace FluentAPI
+{
+    public partial class User
+    {
+        [Column("user_id")]
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public long Age { get; set; }
+        //Навигационное свойство
+        public Company? Company { get; set; } 
+    }
+    //Если не нужно создавать
+    //[NotMapped] 
+    public class Company
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+    //Не связано с DBSet, поэтому не будет создано. Для создания требуется указать, например в OnModelCreating
+    public class Country
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
